29705775981240969869a96c60d2d9fbf4104883
Spencer <samussiah@gmail.com>
2020-02-24 16:39:43 -0500

    fix #12; fix #25; fix #27; fix #28; fix #29

--- src/callbacks/onDraw.js
+++ src/callbacks/onDraw.js
@@ -2,6 +2,7 @@ import makeNestLevel from './onDraw/makeNestLevel';
 import makeDateScale from './onDraw/makeDateScale';
 
 export default function onDraw() {
+    var chart = this;
     chart.listing.wrap.classed('hidden', true);
     chart.wrap.classed('hidden', false);
     chart.controls.wrap.classed('hidden', false);
